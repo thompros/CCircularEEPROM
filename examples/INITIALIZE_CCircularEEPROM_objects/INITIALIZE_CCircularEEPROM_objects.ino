727ec7af29091e2c61b2f336cd4ac46518d4d415
thompros <131445812+thompros@users.noreply.github.com>
2023-05-24 14:15:53 +0300

    Update INITIALIZE_CCircularEEPROM_objects.ino

--- examples/INITIALIZE_CCircularEEPROM_objects/INITIALIZE_CCircularEEPROM_objects.ino
+++ examples/INITIALIZE_CCircularEEPROM_objects/INITIALIZE_CCircularEEPROM_objects.ino
@@ -1,18 +1,18 @@
 #include <CCircularEEPROM.h>
 /*
 We use this sketch to initialize the CCircularEEPROM.
-FIRST OF ALL make sure to set your desired number & values of starting - ending points into header file. The number of these pairs defines the number
-of CCircularEEPROM object instances. An instance's starting point must be always bigger (+1 or more) than the previous instance's ending point to avoid 
+FIRST OF ALL make sure to set your desired number & values of starting - ending points into header file. These pairs define the number
+of CCircularEEPROM objects. An object's starting point must be always bigger (+1 or more) than the previous object's ending point to avoid 
 coverage between their data.
 */
 void setup() {
   Serial.begin(9600);
-  CCircularEEPROM A(CC_START_POINT_1,CC_END_POINT_1,true);   // First instance of CCircularEEPROM object           
+  CCircularEEPROM A(CC_START_POINT_1,CC_END_POINT_1,true);   // Object A. First instance of CCircularEEPROM class          
   A.CC_Scan();
 
 
 
-  //  CCircularEEPROM B(CC_START_POINT_2,CC_END_POINT_2,true);     // Second instance of CCircularEEPROM object     
+  //  CCircularEEPROM B(CC_START_POINT_2,CC_END_POINT_2,true);     // Object B. Second instance of CCircularEEPROM class     
   //  B.CC_Scan();
   
 }
